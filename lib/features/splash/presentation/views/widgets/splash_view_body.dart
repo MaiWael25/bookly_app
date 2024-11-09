c8615fd145c342fc883c1396c06ca323ce091d0a
Mai Wael <maiw8757@gmail.com>
2024-11-09 18:07:58 +0200

    splash view ui

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/core/utils/assests.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [Image.asset(AssestsData.logo)],
+    );
+  }
+}
