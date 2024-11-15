f64130dded2f22d2427e47a80529803927e3f407
Mai Wael <maiw8757@gmail.com>
2024-11-15 13:50:12 +0200

    best seller item part 2

--- lib/features/home/presentaion/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentaion/views/widgets/best_seller_list_view.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:bookly_app/core/utils/assests.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerListView extends StatelessWidget {
@@ -19,6 +22,22 @@ class BestSellerListView extends StatelessWidget {
                       image: AssetImage(AssestsData.testImage),
                       fit: BoxFit.fill)),
             ),
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              )
+            ],
           )
         ],
       ),
