0a224561aadfa85bf9c1a332cd0c80a43d2e7fea
Mai Wael <maiw8757@gmail.com>
2024-11-30 23:25:46 +0200

    create similar books list view

--- lib/features/home/presentaion/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentaion/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/features/home/presentaion/views/widgets/book_rating.d
 import 'package:bookly_app/features/home/presentaion/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentaion/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentaion/views/widgets/custom_book_item.dart';
+import 'package:bookly_app/features/home/presentaion/views/widgets/similar_books_list_view.dart';
 
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
+            padding: EdgeInsets.symmetric(horizontal: width * .28),
             child: const CustomBookImage(),
           ),
           const SizedBox(
@@ -46,6 +47,22 @@ class BookDetailsViewBody extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
           ),
           BooksAction(),
+          const SizedBox(
+            height: 30,
+          ),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.bold),
+              )),
+          const SizedBox(
+            height: 16,
+          ),
+          SimilarBooksListView(),
+          const SizedBox(
+            height: 40,
+          ),
         ],
       ),
     );
