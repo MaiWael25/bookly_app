429d5a3c01dd1106bd9c187798bb631f1f74cd45
Mai Wael <maiw8757@gmail.com>
2024-12-29 12:25:27 +0200

    Display Featured books part 1

---
+++ lib/core/widgets/custom_error_widget.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomError extends StatelessWidget {
+  const CustomError({super.key, required this.errmessage});
+  final String errmessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      errmessage,
+      style: Styles.textStyle18,
+    );
+  }
+}
--- lib/features/home/presentaion/views/widgets/featured_list_view.dart
+++ lib/features/home/presentaion/views/widgets/featured_list_view.dart
@@ -1,21 +1,35 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentaion/manager/featured_books_cubit/featured_books_cubit_cubit.dart';
 import 'package:bookly_app/features/home/presentaion/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeatureBooksListView extends StatelessWidget {
   const FeatureBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: const CustomBookImage(),
-            );
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksCubitState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksCubitSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: const CustomBookImage(),
+                  );
+                }),
+          );
+        } else if (state is FeaturedBooksCubitFailure) {
+          return CustomError(errmessage: state.errMessage);
+        } else {
+          return CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
